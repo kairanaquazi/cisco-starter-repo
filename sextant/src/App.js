01220a50880e8a14789d72c17acb3cb030fd690d
kairanaquazi <kairan.quazi@gmail.com>
2023-02-08 12:21:00 -0800

    task 2

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> Cisco Sextant</h1>
+            </header>
+            <div className="container">
+                <Component header="Test123">
+                    <a href="https://google.com">Click me!</a>
+                </Component>
+                <Component header="Lorem Ipsum">
+                    <a href="https://google.com">Do not click me!</a>
+                </Component>
+                <Component header="Lorem Ipsum">
+                    <a href="https://google.com">Do not click me!</a>
+                </Component>
+                <Component header="Lorem Ipsum">
+                    <a href="https://google.com">Do not click me!</a>
+                </Component>
+                <Component header="Lorem Ipsum">
+                    <a href="https://google.com">Do not click me!</a>
+                </Component>
+                <Component header="Lorem Ipsum">
+                    <a href="https://google.com">Do not click me!</a>
+                </Component>
+            </div>
+        </div>
+    );
+}
+
+function Component(props) {
+    return (
+        <div className="Component">
+            <h2> {props.header} </h2>
+            <div className="ComponentContent"> {props.children}</div>
+        </div>
+    );
 }
 
 export default App;
